baa86ebf1416c1b4379a2df4b1a1e47e1ee6f62d
Matthew Kossa <mdkossa@yahoo.com>
2021-06-01 00:47:26 -0500

    MVP for creating lineups using only CSV file data

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
 # draftkings_clj
 
-FIXME: description
+Algorithm for building optimal lineups in Draft Kings
 
 ## Installation
 
-Download from http://example.com/FIXME.
+N/A
 
 ## Usage
 
-Install dependencies: `lein deps`
-Run: `lein run`
+Install dependencies with `lein deps`
 
-    $ java -jar draftkings_clj-0.1.0-standalone.jar [args]
+Make sure that `DKSalaries.csv` is in the resources directory.
+
+Run with `lein run`
 
 ## Options
 
-FIXME: listing of options this app accepts.
+TODO: Currently no options
 
 ## Examples
 
@@ -28,18 +29,3 @@ FIXME: listing of options this app accepts.
 ### Any Other Sections
 ### That You Think
 ### Might be Useful
-
-## License
-
-Copyright © 2021 FIXME
-
-This program and the accompanying materials are made available under the
-terms of the Eclipse Public License 2.0 which is available at
-http://www.eclipse.org/legal/epl-2.0.
-
-This Source Code may also be made available under the following Secondary
-Licenses when the conditions for such availability set forth in the Eclipse
-Public License, v. 2.0 are satisfied: GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or (at your
-option) any later version, with the GNU Classpath Exception which is available
-at https://www.gnu.org/software/classpath/license.html.
--- src/draftkings_clj/core.clj
+++ src/draftkings_clj/core.clj
@@ -1,12 +1,21 @@
 (ns draftkings-clj.core
   (:gen-class)
   (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.math.combinatorics :as combo]
+            [clojure.pprint :refer [pprint]]
+            [clojure.set :refer [union]]
+            [clojure.walk :refer [keywordize-keys]]))
 
 (defn determine-players-value
-  [players])
+  "Determine a player's value (very simple).
+   To be replaced with something more sophisticated."
+  [players]
+  (map #(assoc % :ratio (/ (:AvgPointsPerGame %) (:Salary %))) players))
 
-(defn csv-data->maps [csv-data]
+(defn csv-data->maps
+  "Convert csv file data into a map."
+  [csv-data]
   (map zipmap
        (->> (first csv-data) ;; First row is the header
             (map keyword) ;; Drop if you want string keys instead
@@ -14,39 +23,166 @@
        (rest csv-data)))
 
 (defn csv-file->map
-  "Open and read a csv file"
+  "Open and read a csv file."
   [filename]
   (with-open [file (io/reader filename)]
     (doall (-> (csv/read-csv file)
                csv-data->maps))))
 
-(defn print-team
-  "Produce a string that provides the team's info."
+(defn format-player
+  "Format a single line for a player on a team."
+  [player]
+  (format "%-8s | %-20s | %-4s | %-15s | %-7d | %-6.2f\n"
+          (:Position player)
+          (:Name player)
+          (:TeamAbbrev player)
+          (subs ((keyword "Game Info") player) 0 15)
+          (:Salary player)
+          (:AvgPointsPerGame player)))
+
+;; TODO: Could be cleaned up a lot
+(defn format-team
+  "Produce a string that provides a team's info."
   [team]
-  (let [longest-name-length (apply max (map #(count (:Name %)) team))]
-  (format "+%8d+%14d")
-  ))
+  (let [team (vec team)
+        longest-name-length (apply max (map #(count (:Name %)) team))
+        team-salary (reduce + (map :Salary team))
+        team-projection (reduce + (map :AvgPointsPerGame team))]
+    (str
+     (format "Team Proj: %.2f\n" team-projection)
+     (format "Team Cost: %d\n" team-salary)
+     (format "%-8s | %-20s | %-4s | %-15s | %-7s | %-6s\n" "Position" "Player" "Team" "Matchup" "Salary" "Proj")
+     (apply str (map #(format-player (get team %)) (range 9))))))
+
+;; TODO: Could handle bad data better (like UNK)
+(defn parse-player-data
+  "Convert data to its correct type."
+  [players]
+  (reduce (fn [coll player]
+            (conj coll (-> player
+                           (update :Salary #(Integer/parseInt %))
+                           (update :AvgPointsPerGame #(Double/parseDouble %)))))
+          []
+          players))
+
+;; TODO: Come up with a way to make this more versatile for other sports / formats
+(defn is-valid-team?
+  "Determine if a team is valid or not."
+  [players]
+  (let [position-count (->> players
+                            (map :Position)
+                            frequencies
+                            keywordize-keys)
+        team-salary (reduce + (map :Salary players))]
+
+    (cond
+      ;; Salary must be $50,000 or less
+      (> team-salary 50000) false
+
+      ;; Must have specific positions filled
+      (not= (:QB position-count) 1) false
+      (not= (:DST position-count) 1) false
+      (< (:RB position-count) 2) false
+      (< (:WR position-count) 3) false
+      (< (:TE position-count) 1) false
+
+      ;; Not all players can be from the same team
+      (< (count (set (map :TeamAbbrev players))) 2) false
+
+      :else true)))
+
+(defn determine-lineups
+  "Determine the best lineup based on projections."
+  [players]
+  ;; Take the top X ratio and top X scoring players
+  ;; TODO: Much cleaner way to do this
+  ;; TODO: Need a more efficient method to pick players
+  (let [top-ratio-players
+        (set (concat
+              (->> players
+                   (filter #(= (:Position %) "QB"))
+                   (sort-by :ratio)
+                   reverse
+                   (take 1))
+              (->> players
+                   (filter #(= (:Position %) "RB"))
+                   (sort-by :ratio)
+                   reverse
+                   (take 2))
+              (->> players
+                   (filter #(= (:Position %) "WR"))
+                   (sort-by :ratio)
+                   reverse
+                   (take 3))
+              (->> players
+                   (filter #(= (:Position %) "TE"))
+                   (sort-by :ratio)
+                   reverse
+                   (take 1))
+              (->> players
+                   (filter #(= (:Position %) "DST"))
+                   (sort-by :ratio)
+                   reverse
+                   (take 1))))
+        top-scoring-players
+        (set (concat
+              (->> players
+                   (filter #(= (:Position %) "QB"))
+                   (sort-by :AvgPointsPerGame)
+                   reverse
+                   (take 1))
+              (->> players
+                   (filter #(= (:Position %) "RB"))
+                   (sort-by :AvgPointsPerGame)
+                   reverse
+                   (take 2))
+              (->> players
+                   (filter #(= (:Position %) "WR"))
+                   (sort-by :AvgPointsPerGame)
+                   reverse
+                   (take 3))
+              (->> players
+                   (filter #(= (:Position %) "TE"))
+                   (sort-by :AvgPointsPerGame)
+                   reverse
+                   (take 1))
+              (->> players
+                   (filter #(= (:Position %) "DST"))
+                   (sort-by :AvgPointsPerGame)
+                   reverse
+                   (take 1))))]
+    ;; Take the set of the top X ratio scoring players build X choose 8
+    (->> (combo/combinations (union top-ratio-players top-scoring-players) 9)
+         (filter is-valid-team?) ;; Make sure team is valid
+         ;; Sort the teams by their projection descending
+         ;; TODO: Fix sort
+         #_(reverse (sort-by (fn [teams]
+                    (map #(reduce + (map :AvgPointsPerGame)) teams)))))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Entry point."
   [& _]
+  ;; TODO: Better way to read file than harcode?
   (let [players (csv-file->map "resources/DKSalaries.csv")]
 
-    ;; TODO: Optionally remove low salary players based on CSV
-    ;; TODO: Optionally remove injured players based on CSV
-    ;; TODO: Handle any values that look invalid in CSV
+    ;; TODO: Add more configuration so less "magic numbers"
+    ;; TODO: Optionally remove low salary players
+    ;; TODO: Optionally remove injured players
+    ;; TODO: Handle any values that look invalid
 
-    (print-team (take 8 players))
-
-    ))
+    (->> players
+         parse-player-data
+         determine-players-value
+         determine-lineups
+         (take 5)
+         (map format-team)
+         (run! println))))
 
 (comment
-  (doall (->> (csv-file->map "resources/DKSalaries.csv")
-              (take 8)
-              (map #(count (:Name %)))
-              (apply max)
-              ))
-
-  (format "+ %8s + %-14s + %-4s + %-15s + %-7s + %-6s +" "Position" "Player" "Team" "Matchup" "Salary" "Proj")
-
+  @(def players (->> (csv-file->map "resources/DKSalaries.csv")
+                     parse-player-data
+                     determine-players-value
+                     determine-lineups
+                     (take 5)
+                     ))
   )
